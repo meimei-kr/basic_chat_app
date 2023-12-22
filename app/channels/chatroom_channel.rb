4fbae5fd9d89cb657d833f8614721ba71ac8e167
meimei-kr <77828683+meimei-kr@users.noreply.github.com>
2023-12-22 16:34:27 +0900

    add: チャネル処理・購読処理を追加

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -1,9 +1,30 @@
 class ChatroomChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    # chatroom_channelからのストリーミングを開始する
+    stream_from "chatroom_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  # クライアントがメッセージ送信に使うメソッド
+  def talk(data)
+    # クライアントからのデータをもとにmessageモデルを組み立てる（現在日時を使用）
+    message = Message.new
+    message.published = Time.now
+    message.sender = data['sender']
+    message.content = data['content']
+    debugger
+    # Messageモデルをレンダリングしてchatroom_channelへブロードキャストする
+    ActionCable.server.broadcast 'chatroom_channel',
+      {message: render_message(message)}
+  end
+
+  private
+
+  def render_message(message)
+    ApplicationController.renderer.render(partial: 'messages/message',
+      locals: { message: message })
   end
 end
--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,6 +1,6 @@
 import consumer from "channels/consumer"
 
-consumer.subscriptions.create("ChatroomChannel", {
+const appChat = consumer.subscriptions.create("ChatroomChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
   },
@@ -10,6 +10,20 @@ consumer.subscriptions.create("ChatroomChannel", {
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    const messages = document.getElementById('messages')
+    messages.insertAdjacentHTML('afterbegin', data['message'])
+  },
+
+  talk(data) {
+    return this.perform('talk', data)
   }
 });
+
+document.getElementById('content').addEventListener('keyup', (e) => {	(4)
+  if (e.key === 'Enter') {
+    const sender = document.getElementById('sender')
+    appChat.talk({sender: sender.value, content: e.target.value});
+    e.target.value = ''
+    e.preventDefault()
+  }
+})
